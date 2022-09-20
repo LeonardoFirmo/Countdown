75ca7f5a00cd271c25ebb472251d57351d2341da
Leonardo Firmo <leonardofirmodamota@gmail.com>
2022-09-20 15:14:33 -0300

    Erro de tempo quebrado após data limite resolvido.

--- script.js
+++ script.js
@@ -2,40 +2,44 @@ let dia = document.querySelector('#dia')
 let hora = document.querySelector('#hora')
 let min = document.querySelector('#min')
 let seg = document.querySelector('#seg')
-const domingo = "18 sep 2022"
+const domingo = "20 sep 2023"
 
 
 
 
 function countDown(){
-    const dataDomingoMadruga = new Date(domingo);
+    const dataLimit = new Date(domingo);
     const currentDate = new Date();
-
-    const totalSeconds = (dataDomingoMadruga - currentDate) / 1000;
+    const totalSeconds = (dataLimit - currentDate) / 1000;
    
     const days = Math.floor(totalSeconds / 3600 / 24)
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
- 
+    
+    if(dataLimit > currentDate){
+        dia.innerHTML=`${days}:`
+        hora.innerHTML=`${formatTime(hours)}:`
+        min.innerHTML=`${formatTime(minutes)}:`
+        seg.innerHTML=`${formatTime(seconds)}`
+    
 
-    dia.innerHTML=`${days}:`
-    hora.innerHTML=`${formatTime(hours)}:`
-    min.innerHTML=`${formatTime(minutes)}:`
-    seg.innerHTML=`${formatTime(seconds)}`
-
-
-  
-
+    }else{
+        clearInterval(atualizaHora)
+        
+        dia.innerHTML=`0:`
+        hora.innerHTML=`00:`
+        min.innerHTML=`00:`
+        seg.innerHTML=`00`
+    }
+   
 }
 
 function formatTime(time){
+    
     return time < 10 ? `0${time}` : time;
 }
 
-
-
-countDown();
 
 let atualizaHora = setInterval(countDown, 1000)
 
